0da17853f7f08397538750483dd00d94c0836397
anne rubio <rubio.anne@gmail.com>
2023-03-29 22:24:47 +0200

    finished verification empty fields

--- app.js
+++ app.js
@@ -1,26 +1,37 @@
 const form = document.querySelector('form');
 const input = form.querySelectorAll('input');
-// const button = document.getElementById('button');
 // console.log(form, input);
 
+// making a function to check for Blank fields so it displays to the user
+// an error message + add to the input a border colored when input is filled
+// and a red color border when input is left blank.
 function checkForEmptyInput(event) {
   const inputThatGetClicked = event.currentTarget;
   // console.log(event.currentTarget);
   // console.log(inputThatGetClicked.value);
   if (inputThatGetClicked.value == "") {
-    inputThatGetClicked.style.borderColor = "red"; // set the right red
-    // need to add alert text can't be blank below each input left blank
+    inputThatGetClicked.classList.add('inputerrors');
+    const errorMessage = document.createElement('p');
+    errorMessage.textContent = "Cannot be blank";
+    inputThatGetClicked.insertAdjacentElement('afterend', errorMessage);
+    errorMessage.classList.add('inputerrors');
   } else {
-    inputThatGetClicked.style.borderColor = "violet"; // set the right purple
+    inputThatGetClicked.classList.add('inputactive');
+    // inputThatGetClicked.style.borderColor = "violet";
   }
 }
 
-// adding an event listener on input fields and run a function to look for empty
-// fields so when leaving the input error message and adding proper color style
+// adding an event listener on input fields and run the function above
 input.forEach(input => {
   input.addEventListener('blur', checkForEmptyInput)
 });
 
+// Receive error messages if the card number, expiry date, or CVC fields are in
+// the wrong format = !numbers
+
+
+
+// Form events short list:
 // Keydown = to prevent specific characters
 // focus = when entering an input
 // blur = when leaving an input
